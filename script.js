7b46b5c5c24680732be4b061715d281252bb17f7
Avanishkm <kavanish101998@gmail.com>
2023-07-01 17:06:15 +0530

    message

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-class API{
+class API {
     #secure;
-
-    constructor(url, method = 'GET'){
-        this.url = url;
-        this.method = method;
-        this.#setSecure();
+  
+    constructor(url, method = 'GET') {
+      this.url = url;
+      this.method = method;
+      this.#setSecure();
     }
-
-    isSecure(){
-        return this.#secure;
+  
+    isSecure() {
+      return this.#secure;
     }
-
-    updateUrl(url){
-        this.url = url;
-        this.#setSecure();
+  
+    updateUrl(url) {
+      this.url = url;
+      this.#setSecure();
     }
-
-    #setSecure(){
-        this.#secure = this.url.startsWith('https');
+  
+    #setSecure() {
+      this.#secure = this.url.startsWith('https');
     }
-}
-
-export default API;
+  }
+  
+  module.exports = API;
 
 const s = new API('http://api.com/api/hello')
 console. log(s.isSecure()) // false
 
-s.updateUrl('https://api. com/api/hello')
+s.updateUrl('https://api.com/api/hello')
 console. log(s.isSecure()) // true
 
 console.log(s.url) // https://api. com/api/hello
